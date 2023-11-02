5be4ce03c6d03b738161dccdc66ef27607e3a94a
chopinrlz <chopinrlz@gmail.com>
2023-11-02 12:23:00 -0400

    Optimized completed scans

--- Scanner.cs
+++ Scanner.cs
@@ -6,12 +6,66 @@ using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace PowerPorts {
+
+    /// <summary>
+    /// Maps a TCP/IP service port to a service name.
+    /// </summary>
+    public enum TcpService {
+        Echo = 7,
+        FtpData = 20,
+        Ftp = 21,
+        Ssh = 22,
+        Telnet = 23,
+        Smtp = 25,
+        Dns = 53,
+        Http = 80,
+        Kerberos = 88,
+        Pop3 = 110,
+        HttpTls = 443,
+        Smb = 445,
+        SqlServer = 1433,
+        HttpInternal = 8080
+    }
+
+    /// <summary>
+    /// Encapulates a port scan result to one server and one port.
+    /// </summary>
     public struct TcpScannerResult {
+
+        /// <summary>
+        /// The IP address of the host.
+        /// </summary>
         public string Server;
+
+        /// <summary>
+        /// The port number scanned.
+        /// </summary>
         public int Port;
+
+        /// <summary>
+        /// The response code: 0 for open, 1 for closed.
+        /// </summary>
         public int Code;
+
+        /// <summary>
+        /// The result string, open or closed.
+        /// </summary>
         public string Result;
 
+        /// <summary>
+        /// The service name typically found on the port.
+        /// </summary>
+        public TcpService Service {
+            get {
+                return (TcpService)Port;
+            }
+        }
+
+        /// <summary>
+        /// Determines of two results are the same.
+        /// </summary>
+        /// <param name="other">A reference to the other result.</param>
+        /// <returns>True of the results are identical, otherwise false.</returns>
         public override bool Equals( object other ) {
             if( other == null ) {
                 return false;
@@ -28,40 +82,91 @@ namespace PowerPorts {
             }
         }
 
+        /// <summary>
+        /// Formats this result as a string including the server and port which can be used to
+        /// index these results in a dictionary.
+        /// </summary>
+        /// <returns>The server and port as a formatted string separated by a hyphen.</returns>
+        /// <example>If the server and port are 127.0.0.1 and 80, this returns "127.0.0.1-00080".</example>
         public override string ToString() {
             return string.Format( "{0}-{1:00000}", Server, Port );
         }
 
+        /// <summary>
+        /// Converts the string value of this result to a hashcode.
+        /// </summary>
+        /// <returns>The hashcode of the string output.</returns>
         public override int GetHashCode() {
             return ToString().GetHashCode();
         }
     }
 
+    /// <summary>
+    /// Encapsulates a TcpClient port scan session with a client, result, and processing flag.
+    /// </summary>
     public class TcpScannerSession {
+
+        /// <summary>
+        /// Gets or sets the client object for this session.
+        /// </summary>
         public TcpClient Client {
             get; set;
         }
 
+        /// <summary>
+        /// Gets or sets a flag to determine if this scanner is still attempting the connection.
+        /// </summary>
         public bool Processing {
             get; set;
         }
 
+        /// <summary>
+        /// Gets or sets the result for the session.
+        /// </summary>
         public TcpScannerResult Result {
             get; set;
         }
+
+        public static string GenerateKey( string server, int port ) {
+            return string.Format( "{0}-{1:00000}", server, port );
+        }
     }
 
+    /// <summary>
+    /// Implements an asynchronous, thread-safe TCP/IP port scanner. 
+    /// </summary>
     public class TcpScanner {
-        private ConcurrentBag<TcpScannerResult> _results;
-        private AsyncCallback _connectCallback;
-        private ConcurrentDictionary<string,TcpScannerSession> _sessions;
 
+        #region Fields
+        /// <summary>
+        /// The collection of scan results.
+        /// </summary>
+        private ConcurrentBag<TcpScannerResult> _results;
+
+        /// <summary>
+        /// The callback delegate for clients to invoke.
+        /// </summary>
+        private AsyncCallback _connectCallback;
+
+        /// <summary>
+        /// The collection of scan sessions.
+        /// </summary>
+        private ConcurrentDictionary<string,TcpScannerSession> _sessions;
+        #endregion
+
+        /// <summary>
+        /// Constructs a new port scanner.
+        /// </summary>
         public TcpScanner() {
             _results = new ConcurrentBag<TcpScannerResult>();
             _connectCallback += new AsyncCallback( ConnectCallback );
             _sessions = new ConcurrentDictionary<string,TcpScannerSession>();
         }
 
+        /// <summary>
+        /// Indicates if any port scan connections are still processing. Poll this flag to determine
+        /// when the batch of scans you request have all completed.
+        /// </summary>
         public bool IsProcessing {
             get {
                 bool processing = false;
@@ -78,13 +183,27 @@ namespace PowerPorts {
             }
         }
 
-        public void Connect( string server, int port ) {
-            var key = string.Format( "{0}-{1:00000}", server, port );
+        /// <summary>
+        /// Starts a port scan of a server and port. This method does not block.
+        /// </summary>
+        /// <param name="server">The IP address of the server.</param>
+        /// <param name="port">The port number.</param>
+        public void StartScan( string server, int port ) {
+            // Generate a session key
+            var key = TcpScannerSession.GenerateKey( server, port );
+
+            // Check active scan sessions and skip if we have a duplicate
             if( _sessions.ContainsKey( key ) ) {
                 return;
             }
-            var target = IPAddress.Parse(server);
+
+            // Parse the IP address
+            var target = IPAddress.Parse( server );
+
+            // Create a TCP connection
             TcpClient client = new TcpClient();
+
+            // Construct the session and assign parameters
             var session = new TcpScannerSession() {
                 Client = client,
                 Processing = true,
@@ -95,17 +214,45 @@ namespace PowerPorts {
                     Result = "connecting"
                 }
             };
+
+            // Register the session for watch
             _sessions.GetOrAdd( key, session );
-            var result = client.BeginConnect( target, port, _connectCallback, session );
+
+            // Start the port scan
+            client.BeginConnect( target, port, _connectCallback, session );
         }
 
-        public void ConnectCallback( IAsyncResult asr ) {
+        /// <summary>
+        /// Occurs when the port scanner completes a connection attempt.
+        /// </summary>
+        /// <remarks>This is the async callback for TcpClient.BeginConnect.</remarks>
+        /// <param name="asr">The callback result.</param>
+        private void ConnectCallback( IAsyncResult asr ) {
+            // Fetch the connection information
             var session = asr.AsyncState as TcpScannerSession;
-            session.Processing = false;
             var result = session.Result;
-            result.Code = 0;
-            result.Result = "complete";
+            var key = TcpScannerSession.GenerateKey( result.Server, result.Port );
+
+            // Call end connect and check the result
+            try {
+                session.Client.EndConnect( asr );
+                result.Code = 0;
+                result.Result = "open";
+            } catch {
+                result.Code = 1;
+                result.Result = "closed";
+            }
+
+            // Add the port scan result to the collection
             _results.Add( result );
+
+            // Kill the client
+            session.Client.Dispose();
+            session.Client = null;
+
+            // Notify callers of completion
+            _sessions.TryRemove( key, out session );
+            session.Processing = false;
         }
     }
 }
--- Test-Scanner.ps1
+++ Test-Scanner.ps1
@@ -1,12 +1,20 @@
+Write-Host "Creating an array of ports to test"
+$ports = 21,22,23,80,443,445,1433,8080,15430
+Write-Host "Importing the port scanner source code"
 $scanner = Get-Content "$PSScriptRoot\Scanner.cs" -Raw
+Write-Host "Compiling the port scanner"
 Add-Type -TypeDefinition $scanner -Language CSharp
+Write-Host "Creating the port scanner object"
 $x = New-Object -TypeName "PowerPorts.TcpScanner"
-$x.Connect( "127.0.0.1", 5534 )
+Write-Host "Initiating a scan of all ports"
+$ports | % { $x.StartScan( "127.0.0.1", $_ ) }
+Write-Host "Waiting for all connection attempts to complete"
 while( $true ) {
     if( $x.IsProcessing ) {
-        Start-Sleep 1
+        Start-Sleep 2
     } else {
         break;
     }
 }
+Write-Host "Reporting connection results"
 ($x.Results) | Format-Table
